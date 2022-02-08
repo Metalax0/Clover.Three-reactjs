82f9660fd6646066b34414bd3a4b4f02fe3820b6
Avash <avash115@gmail.com>
2022-02-08 13:32:11 +0545

    Created component for Word Counter and Coin Flip in HomePageContent

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import MainContainer from "./Sampanna/MainContainer";
 import HomeTodoLeft from "./Sampanna/Home_TODO_Left";
 import HomeTodoRight from "./Sampanna/Home_TODO_right";
 import HomePageContent from "./Avash/HomePageContent";
+import Home_WCounter_Left from "./Avash/Home_WCounter_Left";
+import Home_WCounter_Right from "./Avash/Home_WCounter_Right";
 
 
 const App = () =>{
@@ -9,6 +11,7 @@ const App = () =>{
     <div className="App">
       {/* Navigation Bar Here */}
       <HomePageContent />
+      <MainContainer left_content = {<Home_WCounter_Left />} right_content = {<Home_WCounter_Right />} />
       <MainContainer left_content = {<HomeTodoLeft />} right_content = {<HomeTodoRight />} /> 
       {/* <MainContainer left_content = {} right_content = {} />  */}
       {/* <MainContainer left_content = {} right_content = {} />   */}
---
+++ src/Avash/Home_WCounter_Left/index.js
@@ -0,0 +1,15 @@
+const Home_WCounter_Left = () => {
+    return (
+        <>
+            {/* Left Column Container */}
+            <div className="column-left">
+                {/* Main content Image  */}
+                <div className="image-container">
+                    <img className = "main_img" src='Resources/17.jpg' />
+                </div>
+            </div>
+        </>
+    );
+}
+
+export default Home_WCounter_Left;
