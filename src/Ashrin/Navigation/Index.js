4a4f46035e7bf284e3bd2bb104c30f85b720fdcb
Metalax <sampannapokharel@gmail.com>
2022-02-17 09:35:04 +0545

    minor changes to Metalax/Sampanna page

--- src/Ashrin/Navigation/Index.js
+++ src/Ashrin/Navigation/Index.js
@@ -1,43 +1,44 @@
-import React from 'react';
-import {Link} from 'react-router-dom';
+import { Link } from "react-router-dom";
+
 function Navigation(props) {
-  return (<div>
-     <nav className="flex-navbar">
-            {/* <!-- NavBar Logo --> */}
-            <div className="image-navbar">
-                {/* <!-- <img src="../resources/logo1.png"> --> */}
-                <div className="navbar-logo-text">
-                    <span className="text-color">CLO</span><span className="text-no-color">VER</span>    
-                </div>      
-            </div>
+  return (
+    <div>
+      <nav className="flex-navbar">
+        {/* <!-- NavBar Logo --> */}
+        <div className="image-navbar">
+          {/* <!-- <img src="../resources/logo1.png"> --> */}
+          <div className="navbar-logo-text">
+            <span className="text-color">CLO</span>
+            <span className="text-no-color">VER</span>
+          </div>
+        </div>
 
-            {/* <!-- NavBar Menu Items --> */}
-            <div className="flex-navmenu">
-                {/* <!-- Navigation Bar Menu Item 1 --> */}
-                <div>
-                    < Link to='/' className = "active_menu"> Home </Link>
-                </div>
+        {/* <!-- NavBar Menu Items --> */}
+        <div className="flex-navmenu">
+          {/* <!-- Navigation Bar Menu Item 1 --> */}
+          <div>
+            <Link to="/"> Home </Link>
+          </div>
 
-                {/* <!-- Navigation Bar Menu Item 2 --> */}
-                <div>
-                    <Link to='/avash'> Dr Senior </Link>
-                </div>
+          {/* <!-- Navigation Bar Menu Item 2 --> */}
+          <div>
+            <Link to="/avash"> Dr Senior </Link>
+          </div>
 
-                {/* <!-- Navigation Bar Menu Item 3 --> */}
-                <div>
-                    <Link to='/sampanna'> Metalax </Link>
-                </div>
+          {/* <!-- Navigation Bar Menu Item 3 --> */}
+          <div>
+            <Link to="/sampanna">Metalax </Link>
+          </div>
 
-                {/* <!-- Navigation Bar Menu Item 4 --> */}
-                <div>
-                    <Link to='/ashrin'> Ashrin </Link>
-                </div>
-
-            </div>
-        </nav>
-        {props.children}
-  </div>);
+          {/* <!-- Navigation Bar Menu Item 4 --> */}
+          <div>
+            <Link to="/ashrin"> Ashrin </Link>
+          </div>
+        </div>
+      </nav>
+      {props.children}
+    </div>
+  );
 }
 
 export default Navigation;
-
